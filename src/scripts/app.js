4d126bb248e125dca770437bdbda5f35d4f4c1b5
marcpikachu <forcervp0206@gmail.com>
2025-07-30 11:13:34 +0800

    建構一個假的認養區塊

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,23 @@
 import "@fortawesome/fontawesome-free/css/all.css"
 // 實作寫在這裡！
+import Alpine from "alpinejs";
 
+window.Alpine = Alpine;
+
+Alpine.data("shoppingCart", () => ({
+    cats:[
+      {name:'老大', price:20, pic:'cat001.jpg'},
+      {name:'貝貝', price:15, pic:'cat002.jpg'},
+      {name:'老虎', price:10, pic:'cat003.jpg'}
+      ],
+    adoptList: [],
+
+ addToCart(cat) {
+  console.log("你剛剛認養了：", cat.name);
+  this.adoptList.push(cat);
+  console.log("現在清單是：", this.adoptList);
+}
+
+}))
+
+Alpine.start();
